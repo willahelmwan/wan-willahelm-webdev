a5e510c44ee0de953dbed0285e9cd9b50ca53ed4
williamwan <wan.wi@husky.neu.edu>
2017-07-06 17:02:52 -0400

    modified server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var app = require('./express');
-var express = app.express;
+var express = require('express');
+var app = express();
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require("./test/app");
+require ("./test/app.js")(app);
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);

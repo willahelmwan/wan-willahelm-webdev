49169c711677b6c213fc7949f1a2c1a969e66434
williamwan <wan.wi@husky.neu.edu>
2017-07-06 17:14:46 -0400

    app.js modified

--- test/app.js
+++ test/app.js
@@ -9,7 +9,7 @@ module.exports = function(app)
         var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
         var password = process.env.MLAB_PASSWORD_WEBDEV;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds151232.mlab.com:51232/heroku_z06hhkkx'; // user yours
     }
 
     var mongoose = require("mongoose");

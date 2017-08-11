7c05a007fce7b0786903b4cf773f45541cc01bd2
williamwan <wan.wi@husky.neu.edu>
2017-08-10 21:55:08 -0400

    edited page.schema

--- public/project/server/models/page/page.schema.server.js
+++ public/project/server/models/page/page.schema.server.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var widgetSchema = require('../widget/widget.schema.server');
 var pageSchema = mongoose.Schema({
     _website: {type: mongoose.Schema.Types.ObjectId, ref: "WebsiteModel"},
     name: String,

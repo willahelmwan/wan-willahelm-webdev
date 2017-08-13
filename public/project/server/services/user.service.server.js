138662d469c22209acd36b5a5057528a4303b935
williamwan <wan.wi@husky.neu.edu>
2017-08-13 15:56:54 -0400

    unregistered user login fixed.

--- public/project/server/services/user.service.server.js
+++ public/project/server/services/user.service.server.js
@@ -6,15 +6,7 @@ passport.use(new LocalStrategy(localStrategy));
 passport.serializeUser(serializeUser);
 passport.deserializeUser(deserializeUser);
 
-// var users=[
-//     {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder" , isAdmin: true },
-//     {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-//     {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-//     {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-// ];
-
-
-app.post("/api/project/login", passport.authenticate('local'), login);
+app.post("/api/project/login", login);
 
 
 // http handlers
@@ -35,94 +27,89 @@ function deserializeUser(user, done) {
     userModel
         .findUserById(user._id)
         .then(
-            function(user){
+            function (user) {
                 done(null, user);
             },
-            function(err){
+            function (err) {
                 done(err, null);
             }
         );
 }
 
-function localStrategy(username, password, done){
+function localStrategy(username, password, done) {
     userModel
         .findUserByCredentials(username, password)
-        .then(function(user){
+        .then(function (user) {
             if (!user) {
-                return done(null, false);
+                return done(null, false, {message: "User and password combination not found."});
             }
-            loggedin=user;
             return done(null, user);
-        }, function(err){
-            if (err){
+        }, function (err) {
+            if (err) {
                 return done(err);
             }
         });
 }
-//
-// if(user===null){
-//     userModel
-//         .findUserByUsername(username)
-//         .then(function(user){
-//             if(user===null){
-//                 response.send("2");
-//                 return;
-//             }else{
-//                 response.send("0");
-//                 return;
-//             }
-//         });
-// }else{
-//     loggedin = user;
-//     response.send(user);
-//     return;
-// }
 
-function login(req, res){
-    var user = req.user;
-    res.json(user);
+
+function login(req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.json({success: false, message: info.message});
+        }
+        req.login(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            return res.json({success: true, message: 'authentication succeeded'});
+        });
+    })(req, res, next);
 }
 
-function logoutUser(req, res){
+
+function logoutUser(req, res) {
     req.logOut();
     res.sendStatus(200);
 }
 
-function checkLoggedIn(req, res){
+function checkLoggedIn(req, res) {
     res.send(req.isAuthenticated() ? req.user : '0');
 }
 
-function deleteUser(req, res){
+function deleteUser(req, res) {
     var userId = req.params.userId;
     userModel
         .deleteUser(userId)
-        .then(function(doc){
+        .then(function (doc) {
             res.send(doc);
-        }, function(err){
+        }, function (err) {
             res.send(err);
         });
 }
 
-function updateUser(req, res){
+function updateUser(req, res) {
     var userId = req.params.userId;
     var user = req.body;
     userModel
         .updateUser(userId, user)
-        .then(function(doc){
+        .then(function (doc) {
             res.send(doc);
-        }, function(err){
+        }, function (err) {
             res.send(err);
         });
 }
 
-function createUser(req, res){
+function createUser(req, res) {
     var user = req.body;
     userModel
         .createUser(user)
-        .then(function(user){
-            if(user){
-                req.login(user, function(err) {
-                    if(err) {
+        .then(function (user) {
+            if (user) {
+                req.login(user, function (err) {
+                    if (err) {
                         res.status(400).send(err);
                     } else {
                         res.json(user);
@@ -133,34 +120,34 @@ function createUser(req, res){
 }
 
 
-function findUser(req, response){
+function findUser(req, response) {
     var username = req.query.username;
     userModel
         .findUserByUsername(username)
-        .then(function(user){
-            if(user===null){
+        .then(function (user) {
+            if (user === null) {
                 response.send("2");
                 return;
-            }else{
+            } else {
                 response.send("0");
                 return;
             }
         });
 }
 
-function getAllUsers (req, response){
+function getAllUsers(req, response) {
     userModel
         .findAllUser()
-        .then(function(users){
+        .then(function (users) {
             response.send(users);
         });
 }
 
-function getUserById(req, response){
+function getUserById(req, response) {
     var userId = req.params['userId'];
     userModel
         .findUserById(userId)
-        .then(function(user){
+        .then(function (user) {
             response.json(user);
         });
 }
